45fa906586a01768c76e338c33e9d085811c82bd
jcowl <jcowl@JUSTINSPC>
2026-05-26 18:24:11 -0400

    i swear i saved the scene and pushed but i guess i didnt or something went wrong cause all in class work gone T_T. heres the codig gym thoe

---
+++ Assets/Scripts/Pivot.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Pivot : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentMousePosition = Mouse.current.position.ReadValue();
+        Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(currentMousePosition);
+        worldMousePosition.z = 0f;
+
+
+        bool leftMouse = Mouse.current.leftButton.IsPressed();
+
+        bool leftMousePressed = Mouse.current.leftButton.wasPressedThisFrame;
+        bool leftMouseReleased = Mouse.current.leftButton.wasReleasedThisFrame;
+
+        //Direction to B from A: B-A
+        Vector3 directionToTarget = worldMousePosition - transform.position;
+        transform.right = directionToTarget;
+
+    }
+}
